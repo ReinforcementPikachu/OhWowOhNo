888dcafbbe7590d054e483a9ceebe8e09e148961
Anna Larouche <annalarouche@Annas-Air.hsd1.wa.comcast.net>
2022-12-05 13:24:51 -0800

    beginning coding front and backend

--- client/App.jsx
+++ client/App.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import { store } from './app/store'
-import { Provider } from 'react-redux'
+import { Route, Routes, Link } from 'react-router-dom';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+
+export const App = () => {
+    // return (
+    //     <div>
+    //         <NavBar />
+    //         <div>
+    //             <Routes>
+    //                 <Route path=Signin></Route>
+    //                 <Route path=SignUp>
+    //                 <Route path=Fridge></Route>
+    //                     <routes>
+    //                         <yourfridge></yourfridge>
+    //                         <yourrecies></yourrecies>
+    //                     </routes>
+    //             </Routes>
+    //         </div> 
+    //     </div>
+    // )
+}
