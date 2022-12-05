f352fc6fd4741bfadf4df22df4c04d41facc3c19
labeesley <cluelessclaptrp@gmail.com>
2022-12-05 18:54:58 -0500

    set up routes

---
+++ server/routes/fridgeRouter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const fridgeController = require('../controllers/fridgeController');
+
+const fridgeRouter = express.Router();
+
+//bring in db
+// const Fridge = require('../models/FridgeModel');
+
+
+
+module.exports = fridgeRouter;
---
+++ server/routes/userRouter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+
+const userRouter = express.Router();
+
+//bring in db
+// const User = require('../models/UserModel');
+
+
+
+module.exports = userRouter;
