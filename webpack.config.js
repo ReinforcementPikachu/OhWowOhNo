b2833d020e52ec31452ab84c231e431606d0eccd
Anna Larouche <annalarouche@Annas-Air.hsd1.wa.comcast.net>
2022-12-05 11:49:13 -0800

    basic setup - includes webpack, starter files

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: process.env.NODE_ENV,
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, './Client/index.html'),
+            filename: 'index.html',
+          })],
+    entry: './Client/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        publicPath: '/',
+        filename: 'bundle.js',
+      },
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            exclude: /(node_modules)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            }
+        }, {
+            test: /\.s[ac]ss$/i,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
+    },
+    devServer: {
+        proxy: {
+            "/": "http://localhost:3000",
+        },
+        compress: true,
+        port: 8080,
+    }
+};
