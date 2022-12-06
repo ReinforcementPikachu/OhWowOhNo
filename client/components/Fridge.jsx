973eb00e3783e7ebaa116ee7d42b4cb3d3950f86
Anna Larouche <alarouche@gmail.com>
2022-12-06 09:40:29 -0800

    added form clearing on submit, state now properly updating

--- client/components/Fridge.jsx
+++ client/components/Fridge.jsx
@@ -1,21 +1,33 @@
 import React from 'react'
-import { useState, useEffect} from 'react';
+import { useState, useRef} from 'react';
 import ReactDOM from 'react-dom'
 import { useSelector, useDispatch } from 'react-redux';
 import { addItem, deleteItem } from '../features/fridgeSlice'
 
 
 const Fridge = () => {
-    const [food, setFood] = useState();
+    const [newFood, setFood] = useState('');
     const dispatch = useDispatch();
+    const foodRef = useRef(null)
+
+    const handleSubmit = (event) => {
+        dispatch(addItem(newFood))
+        event.preventDefault();
+        foodRef.current.value = '';
+    }
+
+
     return  (
         <div>
-            <input 
-            id='foodItem' 
-            type='text' 
-            placeholder='Add food...' 
-            onChange={e => setFood(e.target.value)}/>
-            <button onClick={() => dispatch(addItem(food))}>Add</button>
+            <form onSubmit={handleSubmit}>
+                <input 
+                ref={foodRef}
+                id='foodItem' 
+                type='text' 
+                placeholder='Add food...' 
+                onChange={e => setFood(e.target.value)}/>
+                <button type='submit'>Add</button>
+            </form>
         </div>
     )
 }
