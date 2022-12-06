f57a117d3d4c9f21cc5f882a83cec57365f4b24c
Anna Larouche <alarouche@gmail.com>
2022-12-06 11:37:45 -0800

    created recipeSlice reducer

--- client/app/store.js
+++ client/app/store.js
@@ -1,10 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit'
-import fridgeReducer from '../features/fridgeSlice'
-import userReducer from '../features/userSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import fridgeReducer from '../features/fridgeSlice';
+import userReducer from '../features/userSlice';
+import recipeReducer from '../features/recipeSlice';
 
 export const store = configureStore({
   reducer: {
     fridge: fridgeReducer,
-    user: userReducer
+    user: userReducer,
+    recipes: recipeReducer,
   },
 })
