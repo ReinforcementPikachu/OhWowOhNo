dd32a248abf9dc398cd6a11c79fb6e0c959558d1
labeesley <cluelessclaptrp@gmail.com>
2022-12-07 12:18:11 -0500

    getFridge and addIngredient are functional

--- server/routes/fridgeRouter.js
+++ server/routes/fridgeRouter.js
@@ -3,23 +3,23 @@ const fridgeController = require('../controllers/fridgeController');
 
 const fridgeRouter = express.Router();
 
-// fridgeRouter.get('/',
-//   fridgeController.getFridge,
-//   (req, res) => {
-//     console.log('I am in the GET fridgeRouter')
-//     res.status(200).json(res.locals.fridge);
-//   }
-// );
+fridgeRouter.get('/:id',
+  fridgeController.getFridge,
+  (req, res) => {
+    console.log('I am in the GET fridgeRouter')
+    res.status(200).json(res.locals.fridge);
+  }
+);
 
-// fridgeRouter.post('/',
-//   fridgeController.addIngredient,
-//   (req, res) => {
-//     console.log('I am in the POST fridgeRouter')
-//     res.status(200).json(res.locals.ingredient);
-//   }
-// );
+fridgeRouter.post('/',
+  fridgeController.addIngredient,
+  (req, res) => {
+    console.log('I am in the POST fridgeRouter')
+    res.status(200).json(res.locals.ingredient);
+  }
+);
 
-// fridgeRouter.delete('/',
+// fridgeRouter.delete('/delete',
 //   fridgeController.deleteIngredient,
 //   (req, res) => {
 //     // console.log('I am in the fridgeRouter')
