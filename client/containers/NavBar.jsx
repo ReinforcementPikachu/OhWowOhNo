baf9f0c194abc27074c22cc1addb13c505b274de
Anna Larouche <alarouche@gmail.com>
2022-12-07 08:28:54 -0800

    added basic styles to app

--- client/containers/NavBar.jsx
+++ client/containers/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     return (
         <div className='navBar'>
             <h1>Oh No? Oh, Wow!</h1>
-            <div>
+            <div className='navButtonWrapper'>
                 <Link to="/signup"><button>Sign Up</button></Link>
                 <Link to="/yourfridge"><button>Your Fridge</button></Link>
             </div>
