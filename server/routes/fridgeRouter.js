dcf5ad86239a5a2efa21a1a69590ce7b9efa22ce
labeesley <cluelessclaptrp@gmail.com>
2022-12-07 10:20:18 -0500

    working on fridge controller

--- server/routes/fridgeRouter.js
+++ server/routes/fridgeRouter.js
@@ -3,9 +3,28 @@ const fridgeController = require('../controllers/fridgeController');
 
 const fridgeRouter = express.Router();
 
-//bring in db
-// const Fridge = require('../models/FridgeModel');
+fridgeRouter.get('/',
+  fridgeController.getFridge,
+  (req, res) => {
+    console.log('I am in the GET fridgeRouter')
+    res.status(200).json(res.locals.fridge);
+  }
+);
 
+fridgeRouter.post('/',
+  fridgeController.addIngredient,
+  (req, res) => {
+    console.log('I am in the POST fridgeRouter')
+    res.status(200).json(res.locals.ingredient);
+  }
+);
 
+// fridgeRouter.delete('/',
+//   fridgeController.deleteIngredient,
+//   (req, res) => {
+//     // console.log('I am in the fridgeRouter')
+//     res.status(200).json(res.locals.ingredient);
+//   }
+// );
 
 module.exports = fridgeRouter;
