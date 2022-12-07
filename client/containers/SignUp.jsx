7c4b45e0838bccf009e2e2c4adf674958d280344
chrishicks430 <chrishicks430@gmail.com>
2022-12-06 20:54:09 -0500

    fixed routes between login and signup pages

--- client/containers/SignUp.jsx
+++ client/containers/SignUp.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { useSelector, useDispatch } from 'react-redux';
 import { logIn, noUser, selectUserId, selectUsername, selectAuthenticated, selectError } from '../features/userSlice';
 import axios from 'axios';
-import { Navigate } from 'react-router-dom';
+import { Navigate, Link } from 'react-router-dom';
 
 const SignUp = () => {
     const newId = useSelector(selectUserId);
@@ -22,8 +22,9 @@ const SignUp = () => {
         if (username==='chris' && pw ==='chris'){
             dispatch(logIn({id:1, username:username}))
         } else {
-            dispatch(noUser())
+            alert('Looks like that username already exists. Please go back to login and try again')
         }
+    }
         // console.log(newUsername,'state of username after')
         // console.log(newId, 'state of id after')
         // axios.post('/api/user', {
@@ -39,6 +40,7 @@ const SignUp = () => {
         // )
     return (
         <div className= 'login-wrapper'>
+            <form onSubmit={signUpHandler}>
             <h1>Please Sign Up Here</h1>
             <input
                 className= 'username'
@@ -54,8 +56,13 @@ const SignUp = () => {
                 placeholder = 'Password'
                 id = 'loginPassword'>
             </input>
+            <button type='submit'>Sign up here!</button>
+            </form>
+            <Link to="/login"><button>Back to Login page</button></Link>
+            {authenticated && (
+                <Navigate to= "/yourfridge" replace = {true}/>
+            )}
         </div>
     )
-  }
 }
 export default SignUp;
